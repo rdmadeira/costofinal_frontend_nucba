fbb78e392198848334cab4c3058f025a9bec5677
rdmadeira <rdmadeira2@gmail.com>
2023-04-03 17:49:33 -0300

    created react app and installed chakra

---
+++ src/chakra/theme.js
@@ -0,0 +1,8 @@
+import { extendBaseTheme } from '@chakra-ui/react';
+import chakraTheme from '@chakra-ui/theme';
+
+const components = chakraTheme.components;
+
+export const theme = extendBaseTheme({
+  components: {},
+});
