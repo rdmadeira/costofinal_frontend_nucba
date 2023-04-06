d0a61b55160d980c16459ce10fdd52251142fb4c
rdmadeira <rdmadeira2@gmail.com>
2023-04-05 23:09:09 -0300

    Resolved onChange useForm unresolv React.fowardRef

--- src/components/forms/Input.js
+++ src/components/forms/Input.js
@@ -2,7 +2,14 @@ import React from 'react';
 import { Input } from '@chakra-ui/react';
 
 const LoginInput = ({ ...attrs }) => {
-  return <Input {...attrs} backgroundColor={'green.50'} variant={'outline'} />;
+  return (
+    <Input
+      {...attrs}
+      backgroundColor={'green.50'}
+      variant={'outline'}
+      required={false}
+    />
+  );
 };
 
 export default LoginInput;
--- src/components/forms/SignUpForm.js
+++ src/components/forms/SignUpForm.js
@@ -8,19 +8,21 @@ import {
   DrawerFooter,
   Button,
   Text,
-  Input,
+  VStack,
 } from '@chakra-ui/react';
-import { useForm } from 'react-hook-form';
-/* import Input from './Input';
- */
+import { Controller, useForm } from 'react-hook-form';
+import Input from './Input';
+
 const SignUpForm = () => {
   const {
     register,
     handleSubmit,
+    control,
     formState: { errors, isSubmitting, isSubmitSuccessful },
-  } = useForm();
+  } = useForm({ mode: 'onSubmit', reValidateMode: 'onChange' });
 
-  const toast = useToast({});
+  const toast = useToast();
+
   const onSubmit = (data) => {
     console.log(data);
 
@@ -42,42 +44,60 @@ const SignUpForm = () => {
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)}>
-        {inputNames.map((name) => {
-          return (
-            <FormControl key={name} isInvalid={errors[name]} isRequired>
-              <FormLabel htmlFor={name}>
-                {name[0].toLocaleUpperCase() + name.slice(1)}
-              </FormLabel>
-              <Input
-                {...register(name, {
-                  required: { value: true, message: 'Campo obligatório!' },
-                })}
-                name={name}
-                id={name}
-                type={
-                  name === 'password'
-                    ? 'password'
-                    : name === 'teléfono'
-                    ? 'tel'
-                    : 'text'
-                }
-              />
+        <VStack spacing={'4'}>
+          {inputNames.map((name) => {
+            console.log(errors[name]);
+            return (
+              <FormControl isInvalid={errors[name]} isRequired key={name}>
+                <FormLabel htmlFor={name}>
+                  {name[0].toLocaleUpperCase() + name.slice(1)}
+                </FormLabel>
+                <Controller
+                  key={name}
+                  name={name}
+                  control={control}
+                  render={({
+                    field: { onChange, onBlur, value = '', name },
+                  }) => (
+                    <Input
+                      {...register(name, {
+                        required: {
+                          value: true,
+                          message: 'Campo obligatório!',
+                        },
+                      })}
+                      name={name}
+                      id={name}
+                      type={
+                        name === 'password'
+                          ? 'password'
+                          : name === 'teléfono'
+                          ? 'tel'
+                          : 'text'
+                      }
+                      onChange={onChange}
+                      onBlur={onBlur}
+                      value={value}
+                    />
+                  )}
+                />
 
-              <FormErrorMessage>
-                {errors[name] && errors[name].message}
-              </FormErrorMessage>
-            </FormControl>
-          );
-        })}
-        <Text>
-          No tenés cuenta?{' '}
-          <button
-            type="button"
-            onClick={() => setisLogin(!isLogin)}
-            style={{ fontWeight: 'bold', color: 'blue' }}>
-            Registrese
-          </button>
-        </Text>
+                <FormErrorMessage>
+                  {errors[name] && errors[name].message}
+                </FormErrorMessage>
+              </FormControl>
+            );
+          })}
+          <Text>
+            No tenés cuenta?{' '}
+            <button
+              type="button"
+              onClick={() => setisLogin(!isLogin)}
+              style={{ fontWeight: 'bold', color: 'blue' }}>
+              Registrese
+            </button>
+          </Text>
+        </VStack>
         <DrawerFooter>
           <Button type="submit" rightIcon={isSubmitting && <Spinner />}>
             Submit
