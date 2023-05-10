25bd941957938d283c685e6010503edcd9bdab24
rdmadeira <rdmadeira2@gmail.com>
2023-05-09 22:33:18 -0300

    create nodemailerTest branch

---
+++ src/nodemailer/config.js
@@ -0,0 +1,18 @@
+/* import nodemailer from 'nodemailer';
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.USER,
+    pass: process.env.P,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
+
+export default transporter;
+ */
